dfaa7a7670dcea78577623fc093d88e4967236b1
Xiao Mu <u7375367@anu.edu.au>
2021-09-11 00:29:04 +0800

    Task #3, #4 finished and passed all the tests.

--- src/comp1110/ass2/IQStars.java
+++ src/comp1110/ass2/IQStars.java
@@ -27,7 +27,70 @@ public class IQStars {
      *      @return True if the string is well-formed
      */
     static boolean isGameStringWellFormed(String gameString) {
-        return false; // FIXME Task 3 (P): determine whether a wizard or piece string is well-formed
+        // FIXME Task 3 (P): determine whether a wizard or piece string is well-formed
+
+        char[] colors = {'r', 'o', 'y', 'g', 'b', 'i', 'p'};
+        char[] numbers = {'0', '1', '2', '3', '4', '5', '6'};
+
+        // Check Length
+        if (gameString.length() != 3 && gameString.length() != 4) { return false;}
+
+        // Check color character
+        for (int i = 0; i < colors.length; i++) {
+
+            // Color valid
+            if (gameString.charAt(0) == colors[i]) { break;}
+            // Color not valid
+            if (i == colors.length - 1) {return false;}
+        }
+
+        // The character now processing.
+        int now = 1;
+        // max - 1 = the maximum number of char[] numbers.
+        int max = 6;
+        if (gameString.charAt(0) == 'r' || gameString.charAt(0) == 'i') { max = 3;}
+
+        // Check rotation value of a piece
+        if (gameString.length() == 4) {
+
+            for (int i = 0; i < max; i++) {
+
+                // Number valid
+                if (gameString.charAt(now) == numbers[i]) { break;}
+                // Number not valid
+                if (i == max - 1) { return false;}
+            }
+            // Only piece have rotation value
+            now += 1;
+        }
+
+        // Processing row
+        now += 1;
+        max = 4;
+        char row = gameString.charAt(now);
+
+        // Check row value
+        for (int i = 0; i < max; i++) {
+
+            // Row valid
+            if (row == numbers[i]) { break;}
+            // Row not valid
+            if (i == max - 1) { return false;}
+        }
+
+        // Check column value
+        if (row == '0' || row == '2') { max = 7;}
+        else { max = 6;}
+        now -= 1;
+        for (int i = 0; i < max; i++) {
+
+            // Column valid
+            if (gameString.charAt(now) == numbers[i]) { break;}
+            // Column not valid
+            if (i == max - 1) { return false;}
+        }
+        // All tests passed
+        return true;
     }
 
     /**
@@ -52,7 +115,106 @@ public class IQStars {
      * @return True if the game state string is well-formed
      */
     public static boolean isGameStateStringWellFormed(String gameStateString) {
-        return false; // FIXME Task 4 (P): determine whether a game state string is well-formed
+        // FIXME Task 4 (P): determine whether a game state string is well-formed
+
+        // Length of string
+        int str_len = 4;
+        // Find numbers of pieces and wizards
+        int piece_num = 0;
+        int wizard_num = 0;
+        // Loop through the gameStateString, for every character:
+        for (int i = 0; i < gameStateString.length(); i++) {
+
+            // If now processing 'W', process wizard strings later
+            if (gameStateString.charAt(i) == 'W') {
+                // If piece number is not exactly n or too much, return false
+                if (piece_num % 4 != 0 || piece_num > 28) { return false;}
+                // Wizard mode on
+                str_len = 3;
+            }
+            // Cumulate numbers of both piece and wizard
+            else {
+                if (str_len == 4) {
+                    piece_num += 1;
+                }
+                else {
+                    wizard_num += 1;
+                }
+            }
+        }
+        // If wizard number is not exactly n, return false
+        if (wizard_num % 3 != 0) { return false;}
+
+        // Transform them to real numbers (rather than number of characters)
+        piece_num /= 4;
+
+        // Check every substring:
+        str_len = 4;
+        char[] colors = {'r', 'o', 'y', 'g', 'b', 'i', 'p'};
+        for (int i = 0; i < gameStateString.length(); i += str_len) {
+
+            // Changing mode from piece to wizard
+            if (str_len == 1) { str_len = 3;}
+            if (gameStateString.charAt(i) == 'W') { str_len = 1;}
+
+            if (str_len != 1) {
+
+                // e.g. string = "r012b033W", subst(i=0) = "r012"
+                // e.g. string = "Wr01r02", subst(i=2) = "r02"
+                String subst = gameStateString.substring(i, i+str_len);
+
+                // Check if the string well-formed
+                if (!isGameStringWellFormed(subst)) { return false;}
+
+                for (int j = i + str_len; j < gameStateString.length(); j += str_len) {
+
+                    // Avoid OutOfBoundsException
+                    if (j > piece_num * 4 - 1 && i < piece_num * 4) {break;}
+
+                    // Another substring just like subst but comes behind, to compare with it
+                    // e.g. string = "r012b033W", subst = "r012", subst2 = "b033"
+                    String subst2 = gameStateString.substring(j,j+str_len);
+
+                    for (int k = 0; k < str_len; k++) {
+
+                        // In case of two piece strings have the same color, return false
+                        if (str_len == 4 && k == 0 && subst.charAt(k) == subst2.charAt(k)) {return false;}
+
+                        // Check order of color
+                        // e.g. subst = "b01", subst2 = "r12",
+                        // color_order(subst) = 4, color_order(subst2) = 0,
+                        // 4 > 0 therefore it's not in the order.
+                        if (k == 0) {
+                            int color_order1 = 0;
+                            int color_order2 = 0;
+                            for (char color : colors) {
+                                if (subst.charAt(k) != color) {
+                                    color_order1++;
+                                } else {
+                                    break;
+                                }
+                            }
+                            for (char color : colors) {
+                                if (subst2.charAt(k) != color) {
+                                    color_order2++;
+                                } else {
+                                    break;
+                                }
+                            }
+                            if (color_order1 > color_order2) {return false;}
+                        }
+
+                    // Check wizardOrder
+                    if (str_len == 3 && subst.charAt(0) == subst2.charAt(0)) {
+                        if (subst.charAt(2) > subst2.charAt(2)) { return false;}
+                        if (subst.charAt(2) == subst2.charAt(2) && subst.charAt(1) > subst2.charAt(1)) { return false;}
+                        }
+                    }
+                }
+            }
+        }
+        // All tests passed
+        return true;
     }
 
     /**
